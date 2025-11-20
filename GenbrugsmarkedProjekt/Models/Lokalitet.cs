7d8ab339477cedcce8d30c04e8d18a6a5c4428ad
SvanteEK <svanteek@icloud.com>
2025-11-20 10:23:41 +0100

    lokaler hardcoded

--- GenbrugsmarkedProjekt/Models/Lokalitet.cs
+++ GenbrugsmarkedProjekt/Models/Lokalitet.cs
@@ -10,4 +10,12 @@ public class Lokaliteter
     
     public string LokalitetNavn { get; set; } = "EAAA";     
     public string? Lokale { get; set; }
+    // Hardcoded liste over lokaler
+    public static List<Lokaliteter> StandardLokaler => new()
+    {
+        new Lokaliteter { Id = ObjectId.GenerateNewId().ToString(), LokalitetNavn = "EAAA", Lokale = "Lokale A.12" },
+        new Lokaliteter { Id = ObjectId.GenerateNewId().ToString(), LokalitetNavn = "EAAA", Lokale = "Lokale B.24" },
+        new Lokaliteter { Id = ObjectId.GenerateNewId().ToString(), LokalitetNavn = "EAAA", Lokale = "Lokale C.03" },
+        new Lokaliteter { Id = ObjectId.GenerateNewId().ToString(), LokalitetNavn = "EAAA", Lokale = "Kantinen" }
+    };
 }
